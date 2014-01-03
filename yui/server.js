af8f7ce11ad8a17d596fdd706eee7e3d4bbf2f66
Brant Olsen <brantolsen@gmail.com>
2014-01-03 15:13:38 -0500

    Port over the fetch methods to nodejs.

--- yui/server.js
+++ yui/server.js
@@ -2,22 +2,36 @@
 var app = require('http').createServer(handler),
     io = require('socket.io').listen(app),
     staticModule = require('node-static');
-    
+var url = require('url');
+
 // Static File Server
 var fileServer = new staticModule.Server('./');
 
 var tools = require('./tools.js');
-var conn = tools.connect_to_database();
-conn.connect();
 
 // Web Server
 app.listen(3080);
- 
+
 // HTTP Server Handler
 function handler (request, response) {
+    var url_parts = url.parse(request.url, true);
+    var query = url_parts.query;
+
     request.addListener('end', function () {
-    	// Return Static File
-        fileServer.serve(request, response);
+        if (request.url.indexOf('yui') < 0) {
+            console.log(request.url);
+        }
+        if (request.url == '/get_collections.php') {
+            tools.get_collections(response);
+        }
+        else if (request.url.indexOf('get_videos') >= 0) {
+            console.log("Getting videos for " + query.collection_id);
+            tools.get_videos(response, query.collection_id);
+        }
+        else {
+            // Return Static File
+            fileServer.serve(request, response);
+        }
     });
 };
 
--- yui/tools.js
+++ yui/tools.js
@@ -6,31 +6,47 @@ var mysql = require('mysql');
 exports.connect_to_database = function() {
     var connection = mysql.createConnection({
         host     : 'localhost',
-        user     : 'nodejsadmin',
+        user     : 'root',
         password : 'makingthemoney',
-        database : 'nodejs'
+        database : 'aladdin'
       });
 
       return connection;
 };
 
-/**
- * Print the high scores as JSON.
- */
-exports.print_scores = function(response) {
-    var connection = this.connect_to_database();
-    
-    connection.connect();
+exports.get_collections = function(response) {
+    var conn = this.connect_to_database();
+    conn.connect();
 
-    connection.query('SELECT * FROM highscores', function(err, rows, fields) {
+    conn.query('SELECT collection_id, name FROM aladdin.collection', function(err, rows, fields) {
         if (err) throw err;
 
         response.writeHead(200, {'Content-Type': 'application/json'});
-        console.log("Print scores: " + JSON.stringify(rows));
         response.write(JSON.stringify(rows));
-        
         response.end();
     });
-    
-    connection.end();
+
+    conn.end();
 };
+
+
+exports.get_videos = function(response, collection_id) {
+    var conn = this.connect_to_database();
+    conn.connect();
+
+    var sql = 'SELECT video_file_name FROM aladdin.video v ' +
+              'JOIN aladdin.collection_videos cv ON v.video_id = cv.video_id ' +
+              'WHERE cv.collection_id = ?';
+    var post = [collection_id];
+    conn.query(sql, post, function(err, rows, fields) {
+        if (err) throw err;
+
+        response.writeHead(200, {'Content-Type': 'application/json'});
+        response.write(JSON.stringify(rows));
+        response.end();
+    });
+
+    conn.end();
+};
+
+
