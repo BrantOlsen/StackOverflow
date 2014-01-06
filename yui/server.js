b8c614d44b2394d43cb3946e75dfde53444d05e6
Brant Olsen <brantolsen@gmail.com>
2014-01-06 12:34:58 -0500

    Convert all the PHP to nodejs and add express.

--- yui/server.js
+++ yui/server.js
@@ -1,39 +1,74 @@
 // Including libraries
-var app = require('http').createServer(handler),
-    io = require('socket.io').listen(app),
-    staticModule = require('node-static');
-var url = require('url');
-
-// Static File Server
-var fileServer = new staticModule.Server('./');
-
+var express = require('express');
+var path = require('path');
 var tools = require('./tools.js');
+var app = express();
+var io = require('socket.io').listen(app);
 
-// Web Server
-app.listen(3080);
+// Allow POST values to be processed.
+app.use(express.bodyParser());
 
-// HTTP Server Handler
-function handler (request, response) {
-    var url_parts = url.parse(request.url, true);
-    var query = url_parts.query;
-
-    request.addListener('end', function () {
-        if (request.url.indexOf('yui') < 0) {
-            console.log(request.url);
-        }
-        if (request.url == '/get_collections.php') {
-            tools.get_collections(response);
-        }
-        else if (request.url.indexOf('get_videos') >= 0) {
-            console.log("Getting videos for " + query.collection_id);
-            tools.get_videos(response, query.collection_id);
+// Handle the routes.
+app.get('/get_collections', function(req, res) {
+    tools.get_collections(res);
+});
+app.get('/get_videos', function(req, res) {
+    tools.get_videos(res, req.query.collection_id);
+});
+app.get('/save_collection', function(req, res) {
+    tools.get_collection_id(req.query.name, function(collection_id) {
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        if (collection_id) {
+            res.write(JSON.stringify({id: collection_id}));
+            res.end();
         }
         else {
-            // Return Static File
-            fileServer.serve(request, response);
+            tools.add_collection(req.query.name, function(collection_id) {
+                res.write(JSON.stringify({id: collection_id}));
+                res.end();
+            });
         }
     });
-};
+});
+app.post('/save_video', function(req, res) {
+    var video_name = req.files.Filedata.name;
+    var dir_name = req.files.Filedata.path;
+    var collection_id = req.body.collection_id;
+
+    tools.get_folder_id(dir_name, function(dir_id) {
+        function do_add(dir_id) {
+            tools.add_video(video_name, dir_id, collection_id, function(video_id) {
+                tools.add_video_collection_item(video_id, collection_id);
+            });
+
+            res.writeHead(200, {'Content-Type': 'text/plain'});
+            res.write('Upload Success');
+            res.end();
+        };
+        if (dir_id) {
+            do_add(dir_id);
+        }
+        else {
+            tools.add_folder(dir_name, function(dir_id) {
+                do_add(dir_id);
+            });
+        }
+    });
+
+    // Save File
+    //fs.readFile(req.files.displayImage.path, function (err, data) {
+        //var newPath = __dirname + "/uploads/uploadedFileName";
+        //fs.writeFile(newPath, data, function (err) {
+            //res.redirect("back");
+        //});
+    //});
+});
+
+// Serve up static files like .html, .css, and .js.
+app.use(express.static(path.join(__dirname, '')));
 
 // Delete this row if you want to see debug messages
 io.set('log level', 1);
+
+// Listen on port 3080.
+app.listen(3080);
--- yui/tools.js
+++ yui/tools.js
@@ -25,11 +25,13 @@ exports.get_collections = function(response) {
         response.write(JSON.stringify(rows));
         response.end();
     });
-
     conn.end();
 };
 
-
+/**
+ * Find all videos for the given collection id and write them as JSON
+ * to the response object.
+ */
 exports.get_videos = function(response, collection_id) {
     var conn = this.connect_to_database();
     conn.connect();
@@ -38,6 +40,7 @@ exports.get_videos = function(response, collection_id) {
               'JOIN aladdin.collection_videos cv ON v.video_id = cv.video_id ' +
               'WHERE cv.collection_id = ?';
     var post = [collection_id];
+
     conn.query(sql, post, function(err, rows, fields) {
         if (err) throw err;
 
@@ -45,8 +48,134 @@ exports.get_videos = function(response, collection_id) {
         response.write(JSON.stringify(rows));
         response.end();
     });
-
     conn.end();
 };
 
+/**
+ * Get a collections id by the given name. Passes the id to the given callback function.
+ */
+exports.get_collection_id = function(collection_name, callback) {
+    var conn = this.connect_to_database();
+    conn.connect();
 
+    var sql = 'SELECT collection_id FROM aladdin.collection WHERE name = ?';
+    var post = [collection_name];
+
+    conn.query(sql, post, function(err, rows, fields) {
+        if (err) throw err;
+
+        if (rows.length > 0) {
+            callback(rows[0].collection_id);
+        }
+        else {
+            callback(null);
+        }
+    });
+    conn.end();
+}
+
+/**
+ * Add a collection with the given name. Passes the collections new id
+ * to the callback function.
+ */
+exports.add_collection = function(collection_name, callback) {
+    var conn = this.connect_to_database();
+    conn.connect();
+
+    var sql = 'INSERT INTO aladdin.collection (name, created_on, owner_id) VALUES (?, NOW(), ?)';
+    var owner_id = 1;
+    var post = [collection_name, owner_id];
+
+    conn.query(sql, post, function(err, ress) {
+        if (err) throw err;
+
+        callback(ress.insertId);
+    });
+    conn.end();
+}
+
+/**
+ * Get the given folder's id by name. Passes the id to the given callback or
+ * passes null to the callback if nothing was found.
+ */
+exports.get_folder_id = function(dir_name, callback) {
+    var conn = this.connect_to_database();
+    conn.connect();
+
+    var sql = 'SELECT fd_id FROM aladdin.file_directory WHERE directory = ?';
+    var post = [dir_name];
+
+    conn.query(sql, post, function(err, rows, fields) {
+        if (err) throw err;
+
+        if (callback) {
+            if (rows.length > 0) {
+                callback(rows[0].fd_id);
+            }
+            else {
+                callback(null);
+            }
+        }
+    });
+    conn.end();
+}
+
+/**
+ * Add the given folder and pass the new id to the callback function.
+ */
+exports.add_folder = function(dir_name, callback) {
+    var conn = this.connect_to_database();
+    conn.connect();
+
+    var sql = 'INSERT INTO aladdin.file_directory (directory) VALUES (?)';
+    var post = [dir_name];
+
+    conn.query(sql, post, function(err, ress) {
+        if (err) throw err;
+
+        if (callback) callback(ress.insertId);
+    });
+    conn.end();
+}
+
+/**
+ *
+ */
+exports.add_video = function(name, dir_id, source_id, callback) {
+    var conn = this.connect_to_database();
+    conn.connect();
+
+    var sql = 'INSERT INTO aladdin.VIDEO (video_file_name, video_file_directory_id, source_collection_id) ' +
+              'VALUES (?, ?, ?)';
+    var post = [name, dir_id, source_id];
+
+    conn.query(sql, post, function(err, ress) {
+        if (err) throw err;
+
+        if (callback) {
+            callback(ress.insertId);
+        }
+    });
+    conn.end();
+}
+
+/**
+ *
+ */
+exports.add_video_collection_item = function(video_id, collection_id, callback) {
+    var conn = this.connect_to_database();
+    conn.connect();
+
+    var sql = 'INSERT INTO aladdin.COLLECTION_VIDEOs (video_id, collection_id) ' +
+              'VALUES (?, ?)';
+    var post = [video_id, collection_id];
+
+    conn.query(sql, post, function(err, ress) {
+        if (err) throw err;
+
+        if (callback) {
+            callback(ress.insertId);
+        }
+    });
+    conn.end();
+}
